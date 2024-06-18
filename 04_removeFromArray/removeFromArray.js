eb7adb1cc4bbf462b6770526f58da70252ee145c
Cody <codyphillips@ymail.com>
2024-06-18 17:33:44 -0500

    That was really hard for something that seems so simple. I might've done it wrong

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...remove) {
+
+  // Loop through args array and compare each item to
+  // each item in the given array, then remove if they are
+  // ===
+  remove.forEach(itemToRemove => {
+    array.forEach((arrayItem, i) => {
+      if (arrayItem === itemToRemove) {
+        array.splice(i, 1);
+      }
+      i++
+    });
+  });
+
+  return array;
 };
 
 // Do not edit below this line
