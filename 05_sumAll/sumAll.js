5a3d199312d3825cccf2a0658ef66ad5f5602aea
Cody <codyphillips@ymail.com>
2024-06-18 16:49:06 -0500

    These are fun challenges

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,11 +4,13 @@ const sumAll = function (num1, num2) {
   let maxNum = 0;
 
   // Error handling
-  if (minNum < 0
-    || typeof minNum !== "number"
-    || typeof maxNum !== "number") {
+  if (num1 < 0
+    || num2 < 0
+    || typeof num1 !== "number"
+    || typeof num2 !== "number") {
     return "ERROR"
   }
+
 
   // Determining which number is min and which is max
   if (num1 < num2) {
@@ -20,6 +22,7 @@ const sumAll = function (num1, num2) {
   } else if (num1 === num2) {
     return num1 + num2;
   }
+
 
   // Executing the function
   for (let i = minNum; i <= maxNum; i++) {
