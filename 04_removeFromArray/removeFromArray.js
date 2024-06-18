554eee1e40c5702515c677b1edf262669386d8e9
Cody <codyphillips@ymail.com>
2024-06-18 17:42:16 -0500

    My code was very bad. But I found out about the filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,16 +3,20 @@ const removeFromArray = function (array, ...remove) {
   // Loop through args array and compare each item to
   // each item in the given array, then remove if they are
   // ===
-  remove.forEach(itemToRemove => {
-    array.forEach((arrayItem, i) => {
-      if (arrayItem === itemToRemove) {
-        array.splice(i, 1);
-      }
-      i++
-    });
-  });
 
-  return array;
+
+  // remove.forEach(itemToRemove => {
+  //   array.forEach((arrayItem, i) => {
+  //     if (arrayItem === itemToRemove) {
+  //       array.splice(i, 1);
+  //     }
+  //     i++
+  //   });
+  // });
+  // return array;
+
+  return array.filter(element => !remove.includes(element));
+
 };
 
 // Do not edit below this line
