3cfcaa4cb312d451f214751517a2f263007ae83a
Cody <codyphillips@ymail.com>
2024-06-19 16:07:05 -0500

    Completed Celsius converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  convertedCelsius = (fahrenheit - 32) * (5 / 9);
+  Math.round(convertedCelsius); // round first decimal
+  return parseFloat(convertedCelsius.toFixed(1))
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  convertedFahrenheit = celsius * (9 / 5) + 32;
+  Math.round(convertedFahrenheit);
+  return parseFloat(convertedFahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
